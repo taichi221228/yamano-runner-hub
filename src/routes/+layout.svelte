c14290e40490168f44ce8de5ecb4cb8403594db6
taichi221228 <taichi221228@icloud.com>
2023-12-10 11:51:15 +0900

    Update AppShell slot and add navigation sidebar
    The AppShell slotSidebarLeft has been updated with CSS properties and a navigation sidebar has been added with the 'Home' link. This modifications are part of a larger effort to improve the site's layout and user navigation experience.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <!-- App Shell -->
-<AppShell>
+<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
   <svelte:fragment slot="header">
     <!-- App Bar -->
     <AppBar>
@@ -38,6 +38,14 @@
         </a>
       </svelte:fragment>
     </AppBar>
+  </svelte:fragment>
+  <svelte:fragment slot="sidebarLeft">
+    <!-- Insert the list: -->
+    <nav class="list-nav">
+      <ul>
+        <li><a href="/">Home</a></li>
+      </ul>
+    </nav>
   </svelte:fragment>
   <!-- Page Route Content -->
   <slot />
