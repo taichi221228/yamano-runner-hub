40e946156051cd441ccc4926796f78926ad65e44
taichi221228 <taichi221228@icloud.com>
2023-12-11 21:23:01 +0900

    Add InputFlyerId to Stepper component
    The InputFlyerId component has been integrated into the Stepper component in the '+page.svelte' file. This new component creates a dedicated section for inputting flyer IDs, providing

---
+++ src/features/link-install/components/InputFlyerId.svelte
@@ -0,0 +1,11 @@
+<script>
+  import CustomStep from '../../../components/CustomStep.svelte';
+  import MainPanel from '../../../components/MainPanel.svelte';
+</script>
+
+<CustomStep>
+  <svelte:fragment slot="header">
+    <h2>Input flyer IDs</h2>
+  </svelte:fragment>
+  <MainPanel></MainPanel>
+</CustomStep>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,15 @@
   import { Step, Stepper } from '@skeletonlabs/skeleton';
   import { store } from '../features/link-install/store';
   import SelectType from '../features/link-install/components/SelectType.svelte';
+  import InputFlyerId from '../features/link-install/components/InputFlyerId.svelte';
 </script>
 
 <div class="card flex h-full w-full flex-col items-center p-6">
   <Stepper class="flex h-full w-full flex-col space-y-6" regionContent="grow">
     <SelectType />
-    <Step class="flex h-full flex-col space-y-10" regionNavigation="!mt-auto">
-      <svelte:fragment slot="header">Get Started!</svelte:fragment>
-      This example Stepper will teach you how to use this component. Tap next to proceed to the next
-      step.
-    </Step>
+    {#if $store.isMultiEdition || $store.isMultiLink || !$store.isMultiPage}
+      <InputFlyerId />
+    {/if}
     {#if $store.isMultiPage}
       <Step class="flex h-full flex-col space-y-10" regionNavigation="!mt-auto">
         <svelte:fragment slot="header">Step 3</svelte:fragment>
