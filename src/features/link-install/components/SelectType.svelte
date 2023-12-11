8591883ee0c3b7586ad96cbf187476944b542b52
taichi221228 <taichi221228@icloud.com>
2023-12-11 21:15:12 +0900

    Adjust padding in SelectType.svelte list items
    Increased vertical padding for each list item in SelectType.svelte. This will improve visual spacing and readability for the displayed list options.

--- src/features/link-install/components/SelectType.svelte
+++ src/features/link-install/components/SelectType.svelte
@@ -9,19 +9,19 @@
   <div class="card bg-surface-50-900-token text-token">
     <ul>
       <li
-        class="mx-4 flex items-center justify-between border-b border-surface-500/[.2] py-2 first:pt-4 last:border-none last:pb-4"
+        class="mx-4 flex items-center justify-between border-b border-surface-500/[.2] py-3 first:pt-4 last:border-none last:pb-4"
       >
         Multiple pages
         <SlideToggle name="slide" bind:checked={$store.isMultiPage} />
       </li>
       <li
-        class="mx-4 flex items-center justify-between border-b border-surface-500/[.2] py-2 first:pt-4 last:border-none last:pb-4"
+        class="mx-4 flex items-center justify-between border-b border-surface-500/[.2] py-3 first:pt-4 last:border-none last:pb-4"
       >
         Multiple links
         <SlideToggle name="slide" bind:checked={$store.isMultiLink} />
       </li>
       <li
-        class="mx-4 flex items-center justify-between border-b border-surface-500/[.2] py-2 first:pt-4 last:border-none last:pb-4"
+        class="mx-4 flex items-center justify-between border-b border-surface-500/[.2] py-3 first:pt-4 last:border-none last:pb-4"
       >
         Multiple editions
         <SlideToggle name="slide" bind:checked={$store.isMultiEdition} />
