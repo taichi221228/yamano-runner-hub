fd08f8f6cd07eb872061f3be5f1974940a6777c2
taichi221228 <taichi221228@icloud.com>
2023-12-11 21:18:51 +0900

    Improve list item padding in SelectType.svelte
    Enhanced the padding for list items inside SelectType.svelte for better visual spacing and readability. This ensures that the layout of options remains visually clear and easy to interact with.

--- src/features/link-install/components/SelectType.svelte
+++ src/features/link-install/components/SelectType.svelte
@@ -6,22 +6,22 @@
 
 <CustomStep>
   <svelte:fragment slot="header">Select a flyer type</svelte:fragment>
-  <div class="card bg-surface-50-900-token text-token">
+  <div class="card bg-surface-50-900-token text-token p-4">
     <ul>
       <li
-        class="mx-4 flex items-center justify-between border-b border-surface-500/[.2] py-3 first:pt-4 last:border-none last:pb-4"
+        class="flex items-center justify-between border-b border-surface-500/[.2] py-3 first:pt-0 last:border-none last:pb-0"
       >
         Multiple pages
         <SlideToggle name="slide" bind:checked={$store.isMultiPage} />
       </li>
       <li
-        class="mx-4 flex items-center justify-between border-b border-surface-500/[.2] py-3 first:pt-4 last:border-none last:pb-4"
+        class="flex items-center justify-between border-b border-surface-500/[.2] py-3 first:pt-0 last:border-none last:pb-0"
       >
         Multiple links
         <SlideToggle name="slide" bind:checked={$store.isMultiLink} />
       </li>
       <li
-        class="mx-4 flex items-center justify-between border-b border-surface-500/[.2] py-3 first:pt-4 last:border-none last:pb-4"
+        class="flex items-center justify-between border-b border-surface-500/[.2] py-3 first:pt-0 last:border-none last:pb-0"
       >
         Multiple editions
         <SlideToggle name="slide" bind:checked={$store.isMultiEdition} />
