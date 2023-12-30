62e030a2d0ed58ebaf429453ce950222fd96d61d
taichi221228 <taichi221228@icloud.com>
2023-12-30 13:38:19 +0900

    Add padding to CustomStep component
    The update adds padding to the top of the CustomStep component in the Svelte application. This change improves the visual spacing, ensuring the layout appears correctly in different scenarios.

--- src/components/CustomStep.svelte
+++ src/components/CustomStep.svelte
@@ -2,7 +2,7 @@
   import { Step } from '@skeletonlabs/skeleton';
 </script>
 
-<Step class="flex h-full flex-col space-y-10" regionNavigation="!mt-auto">
+<Step class="flex h-full flex-col space-y-10" regionNavigation="!mt-auto pt-6">
   <svelte:fragment slot="header">
     <h2><slot name="header" /></h2>
   </svelte:fragment>
